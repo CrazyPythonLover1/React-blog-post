7dd2ede3a50d8c78e9cf1a2a8cd04301c11fdc3b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-05 19:48:38 +0600

    React blog App

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Blog from './components/Blog/Blog';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Container maxWidth="sm">
+       <Router>
+         <Switch>
+           <Route path="/blog">
+            <Blog/>
+           </Route>
+           <Route path="/">
+            <Blog/>
+           </Route>
+         </Switch>
+       </Router>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
